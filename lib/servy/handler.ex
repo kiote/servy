3310d62f45f41a96fda4ed3cac69523599c1fad2
Ekaterina Krivich <ekaterinak@heathmont.net>
2017-10-01 14:22:17 +0200

    step11

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -2,31 +2,51 @@ defmodule Servy.Handler do
     def handle(request) do
       request 
       |> parse 
+      |> log
       |> route 
       |> format_response
     end
 
+    def log(conv), do: IO.inspect conv
+
     def parse(request) do
-      conv = %{method: "GET", path: "/wildthings", resp_body: ""}
+      [method, path, _] = request
+        |> String.split("\n")
+        |> List.first
+        |> String.split(" ")
+      %{method: method, path: path, resp_body: ""}
     end
 
     def route(conv) do
-      conv = %{method: "GET", path: "/wildthings", resp_body: "Bears, lions, tigers"}
+      %{conv | resp_body: "Bears, Lions, Tigers"}
     end
 
     def format_response(conv) do
         """
         HTTP/1.1 200 OK
         Content-Type: text/html
-        Content-Length: 20
+        Content-Length: #{String.length(conv.resp_body)}
 
-        Bears, lions, tigers
+        #{conv.resp_body}
         """
     end
 end
 
 request = """
 GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts response
+
+
+
+request = """
+GET /bears HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
